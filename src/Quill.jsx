0d48227fcc6095c30146558421ac4a7aba7c7f94
Kyungsle <79993356+keinn51@users.noreply.github.com>
2024-09-09 17:54:27 +0900

    Feat: 퀼 에디터 이미지 테스트

--- src/Quill.jsx
+++ src/Quill.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -7,9 +7,40 @@ const Editor = () => {
   const [html, setHtml] = useState('');
   const quillRef = useRef(null); // Reference to the React Quill editor
 
+
+  const handleImageUpload = () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = async () => {
+      const file = input.files[0];
+      const formData = new FormData();
+      formData.append('image', file); // Adjust the field name as needed for your server
+
+      const quill = quillRef.current.getEditor();
+      quill.insertText(0, `<img src="https://via.placeholder.com/150" alt="Uploaded Image" />`);
+    };
+  };
+
+
+  const modules = useMemo(() => ({
+    toolbar: {
+     container: [
+        ['image'], // Add image button
+      ],
+      handlers: {
+        image: handleImageUpload
+      } 
+    }
+  }),[]);
+
+
   const handlePlainText = () => {
     const editor = quillRef.current.getEditor(); // Access the Quill editor
     const plainText = editor.getText(); // Get plain text content
+    console.log(plainText);
     setHtml(plainText);
     // You can set this plain text value in your state or use it as needed
   };
@@ -19,7 +50,9 @@ const Editor = () => {
       <ReactQuill 
         ref={quillRef} 
         value={value} 
+        modules={modules}
         onChange={(e) => {
+          // console.log(e);
           setValue(e);
           handlePlainText(e);
         }}
