4143a12f3939b2c9d58353bd6879bbbc7e0ab23f
Kyungsle <79993356+keinn51@users.noreply.github.com>
2024-09-09 16:45:43 +0900

    Add: Custom editor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Draft from './Draftjs'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Draft from "./Draftjs";
+import Custom from "./Custom";
 
 const Home = () => <h2>Home Page</h2>;
 
@@ -15,12 +16,16 @@ const App = () => {
           <li>
             <Link to="/draft">Draft</Link>
           </li>
+          <li>
+            <Link to="/custom">custom</Link>
+          </li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/draft" element={<Draft />} />
+        <Route path="/custom" element={<Custom />} />
       </Routes>
     </Router>
   );
